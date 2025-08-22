5d0496a6565c121cfddbc644ffb11f544be62165
Veronique Galmiche <vgalmich@f6r4s12.clusters.42paris.fr>
2025-08-22 19:42:01 +0200

    OK

--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -1,30 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*   DiamondTrap.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vgalmich <vgalmich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/22 13:11:55 by vgalmich          #+#    #+#             */
-/*   Updated: 2025/08/22 19:40:48 by vgalmich         ###   ########.fr       */
+/*   Created: 2025/08/22 19:41:30 by vgalmich          #+#    #+#             */
+/*   Updated: 2025/08/22 19:41:31 by vgalmich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FRAGTRAP_HPP
-# define FRAGTRAP_HPP
-
-#include "ClapTrap.hpp"
-
-class FragTrap : virtual public ClapTrap
-{
-    public:
-        FragTrap();
-        FragTrap(std::string Name);
-        FragTrap(const FragTrap &copy);
-        FragTrap &operator=(const FragTrap &other);
-        ~FragTrap();
-
-        void highFivesGuys(void);    
-};
-
-#endif
--- ex03/DiamondTrap.hpp
+++ ex03/DiamondTrap.hpp
@@ -1,30 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*   DiamondTrap.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vgalmich <vgalmich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/22 13:11:55 by vgalmich          #+#    #+#             */
-/*   Updated: 2025/08/22 19:40:48 by vgalmich         ###   ########.fr       */
+/*   Created: 2025/08/22 19:41:16 by vgalmich          #+#    #+#             */
+/*   Updated: 2025/08/22 19:41:16 by vgalmich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FRAGTRAP_HPP
-# define FRAGTRAP_HPP
-
-#include "ClapTrap.hpp"
-
-class FragTrap : virtual public ClapTrap
-{
-    public:
-        FragTrap();
-        FragTrap(std::string Name);
-        FragTrap(const FragTrap &copy);
-        FragTrap &operator=(const FragTrap &other);
-        ~FragTrap();
-
-        void highFivesGuys(void);    
-};
-
-#endif
