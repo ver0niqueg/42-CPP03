fc3b077290f0e34bcae6f634c8bbbb6126884756
Veronique Galmiche <vgalmich@f6r3s1.clusters.42paris.fr>
2025-08-26 12:25:15 +0200

    OK

--- ex03/DiamondTrap.hpp
+++ ex03/DiamondTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: vgalmich <vgalmich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 19:41:16 by vgalmich          #+#    #+#             */
-/*   Updated: 2025/08/25 16:08:29 by vgalmich         ###   ########.fr       */
+/*   Updated: 2025/08/26 12:25:05 by vgalmich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,6 @@
 
 # include "FragTrap.hpp"
 # include "ScavTrap.hpp"
-
-/* DiamondTrap hérite de FragTrap et de ScavTrap qui eux-même derivent de 
-ClapTrap -> deux instances de ClapTrap sont donc créées -> la solution
-c'est de faire un héritage virtuel sur FragTrap et ScavTrap */
 
 class DiamondTrap : public FragTrap, public ScavTrap
 {
@@ -32,7 +28,7 @@ class DiamondTrap : public FragTrap, public ScavTrap
 		DiamondTrap& operator=(const DiamondTrap& other);
 		~DiamondTrap();
 
-		void attack(const std::string& target); // utilise ScavTrap::attack
+		void attack(const std::string& target);
 		void whoAmI();
 	
 };
